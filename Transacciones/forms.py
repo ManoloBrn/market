ee116ccc3c52a6693ffd75d27c5c7f829156c72f
Emmanuel <e.beltranfs@gmail.com>
2016-12-02 12:50:34 -0600

    Form corregido

--- Transacciones/forms.py
+++ Transacciones/forms.py
@@ -1,13 +1,8 @@
 from django import forms
+from .models import *
+from django.forms import ModelForm
 
-class CompraForm(forms.Form):
-	name = forms.CharField(max_length=60)
-	creditCard = forms.CharField(max_length=24)
-	cvc = forms.DecimalField()
-	expDate = forms.DateField()
-	street = forms.CharField(max_length=100)
-	colonia = forms.CharField(max_length=100)
-	city = forms.CharField(max_length=100)
-	estate = forms.CharField(max_length=100)
-	zipcode = forms.IntegerField()
-	country = forms.CharField(max_length=30)
+class CompraForm(ModelForm):
+	class Meta:
+		model = Checkout
+		fields = "__all__"
